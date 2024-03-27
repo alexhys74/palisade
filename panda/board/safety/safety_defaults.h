d028f08b43c7cdbc87cf6a3a703e9b717bae2c9d
alexhys <alexhys74@gmail.com>
2024-03-27 15:44:57 +0900

    핸들 유지모드

--- panda/board/safety/safety_defaults.h
+++ panda/board/safety/safety_defaults.h
@@ -31,18 +31,85 @@ static int nooutput_tx_lin_hook(int lin_num, uint8_t *data, int len) {
 }
 
 static int default_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
-  UNUSED(to_fwd);
+  int addr = GET_ADDR(to_fwd);
   int bus_fwd = -1;
 
-  if (true) {
-      if (bus_num == 0) {
-          bus_fwd = 2;
-      }
-      if (bus_num == 2) {
-          bus_fwd = 0;
-      }
+  if (bus_num == 0 && (Fwd_bus1 || Fwd_bus2 || Fwd_obd)) {
+    if ((Fwd_bus1 || Fwd_obd) && Fwd_bus2) {
+    bus_fwd = 12;
+    } else {
+      bus_fwd = Fwd_bus2 ? 2 : 1;
+    }
   }
+  if (bus_num == 1 && (Fwd_bus1 || Fwd_obd)) {
+    bus_fwd = Fwd_bus2 ? 20 : 0;
+  }
+  if (bus_num == 2 && Fwd_bus2) {
+    bus_fwd = (Fwd_bus1 || Fwd_obd) ? 10 : 0;
+  }
+    // Code for LKA/LFA/HDA anti-nagging.
+  if (addr == 593 && bus_fwd != -1) {
+    uint8_t dat[8];
+    int New_Chksum2 = 0;
+    for (int i=0; i<8; i++) {
+      dat[i] = GET_BYTE(to_fwd, i);
+    }
+    if (MDPS12_cnt > 330) {
+      int StrColTq = dat[0] | (dat[1] & 0x7) << 8;
+      int OutTq = dat[6] >> 4 | dat[7] << 4;
+      if (MDPS12_cnt == 331) {
+        StrColTq -= 164;
+      } else {
+        StrColTq = Last_StrColTq + 34;
+      }
+      OutTq = 2058;
+      dat[0] = StrColTq & 0xFF;
+      dat[1] &= 0xF8;
+      dat[1] |= StrColTq >> 8;
+      dat[6] &= 0xF;
+      dat[6] |= (OutTq & 0xF) << 4;
+      dat[7] = OutTq >> 4;
 
+      uint32_t* RDLR = (uint32_t*)&(to_fwd->data[0]);
+      uint32_t* RDHR = (uint32_t*)&(to_fwd->data[4]);
+
+      *RDLR &= 0xFFF800;
+      *RDLR |= StrColTq;
+      *RDHR &= 0xFFFFF;
+      *RDHR |= OutTq << 20;
+
+      Last_StrColTq = StrColTq;
+      dat[3] = 0;
+      if (!MDPS12_checksum) {
+        for (int i=0; i<8; i++) {
+          New_Chksum2 += dat[i];
+        }
+        New_Chksum2 %= 256;
+      } else if (MDPS12_checksum) {
+        uint8_t crc = 0xFF;
+        uint8_t poly = 0x1D;
+        int i, j;
+        for (i=0; i<8; i++){
+          if (i!=3){ //don't include CRC byte
+            crc ^= dat[i];
+            for (j=0; j<8; j++) {
+              if ((crc & 0x80) != 0U) {
+                crc = (crc << 1) ^ poly;
+              } else {
+                crc <<= 1;
+              }
+            }
+          }
+        }
+        crc ^= 0xFF;
+        crc %= 256;
+        New_Chksum2 = crc;
+      }
+      *RDLR |= New_Chksum2 << 24;
+    }
+    MDPS12_cnt += 1;
+    MDPS12_cnt %= 345;
+  }
   return bus_fwd;
 }
 
